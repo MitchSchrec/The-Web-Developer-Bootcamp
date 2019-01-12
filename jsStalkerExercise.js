a45f0ab814ae5f202d94b8ac12ce08f627ea7dcb
Mitchell Schrecongost <Mitchell@Schrecongost.com>
2019-01-12 11:20:32 -0500

    S10L120: JS Stalker Exercise

---
+++ jsStalkerExercise.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your age in years?");
+
+console.log("Welcome to the website " + userFirstName + " " + userLastName);
+console.log("It looks like you are " + userAge + " years old.");
